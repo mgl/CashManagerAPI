d3ae3f9c487ffa2f47f6cdb2c82dd72a5bb8e214
mgl <marius.glo@proton.me>
2022-11-15 16:36:35 +0100

    Bank Action model

--- src/interfaces/bankAccount.ts
+++ src/interfaces/bankAccount.ts
@@ -1,10 +1,11 @@
-import { Bson } from "../../deps.ts";
+import { Bson } from "../../deps.ts"
 
 export interface BankAccount {
-  _id: Bson.ObjectId;
-  email: string;
-  fisrtname: string;
-  lastname: string;
-  password: string;
-  balance: number;
+  _id: Bson.ObjectId
+  email: string
+  firstname?: string
+  lastname?: string
+  password: string
+  balance: number
 }
+
--- src/models/bankAccount.ts
+++ src/models/bankAccount.ts
@@ -1,91 +1,98 @@
-import { db } from "../db/mongo.ts"
-import { BankAccount } from "../interfaces/bankAccount.ts"
+import { db } from "../db/mongo.ts";
+import { BankAccount } from "../interfaces/bankAccount.ts";
 
-const collectionName: string = Deno.env.get("COLLECTION_NAME") ? Deno.env.get("COLLECTION_NAME")! : "bankAccounts"
+const collectionName: string = Deno.env.get("COLLECTION_NAME")
+  ? Deno.env.get("COLLECTION_NAME")!
+  : "bankAccounts";
 
-const bankAccounts = db.collection<BankAccount>(collectionName)
+const bankAccounts = db.collection<BankAccount>(collectionName);
 
 export default {
-
   /**
-   * Takes in the id params & checks if the bank account item exists
-   * in the database
-   * @param id
-  * @returns boolean to tell if an entry of bank account exists
+   * @description Get all bank accounts count
+   *
+   * @returns number of bank accounts
    */
-  doesExistById: async ({ _id }: BankAccount) => {
-    const count = await bankAccounts.estimatedDocumentCount({
+  getCount: async () => {
+    return await bankAccounts.estimatedDocumentCount({
       _id: { $ne: null },
-    })
-    return count > 0
+    });
   },
 
   /**
-   * Will return all the entries
+   * @description Will return all the entries
+   *
    * @returns array of bank account entries
    */
   getAll: async () => {
-    return await bankAccounts.find({ username: { $ne: null } }).toArray()
+    return await bankAccounts.find({ username: { $ne: null } }).toArray();
   },
 
   /**
-   * Takes in the id params & returns the bank account item found
-   * against it.
+   * @description Takes in the id params & returns the bank account item found
+   *
    * @param id
+   *
    * @returns object of todo item
    */
   getById: async ({ _id }: BankAccount) => {
-    return await bankAccounts.findOne({ _id: _id })
+    return await bankAccounts.findOne({ _id: _id });
   },
 
   /**
-   * Adds a new todo item to todo table
-   * @param todo
-   * @param isCompleted
+   * @description Adds a new bank account to the database
+   *
+   * @param bankAccount
+   *
+   * @returns object of bank account item
+   * @returns null if bank account already exists
+   * @returns null if bank account is not added
    */
-  add: async (
-    { todo, isCompleted }: BankAccount,
-  ) => {
-    return await client.query(
-      `INSERT INTO ${TABLE.TODO}(todo, isCompleted) values(?, ?)`,
-      [
-        todo,
-        isCompleted,
-      ],
-    )
+  add: async (bankAccount: BankAccount) => {
+    const { _id } = bankAccount;
+    const bankAccountExists = await bankAccounts.findOne({ _id: _id });
+    if (bankAccountExists) {
+      return null;
+    }
+    const { $oid } = await bankAccounts.insertOne(bankAccount);
+    return await bankAccounts.findOne({ _id: $oid });
   },
 
   /**
-   * Updates the content of a single todo item
-   * @param id
-   * @param todo
-   * @param isCompleted
-   * @returns integer (count of effect rows)
+   * @description Updates the content of a single bank account
+   *
+   * @param _id
+   *
+   * @returns object of bank account item
+   * @returns null if bank account does not exist
+   * @returns null if bank account is not updated
    */
-  updateById: async ({ _id, todo, isCompleted }: BankAccount) => {
-    const result = await client.query(
-      `UPDATE ${TABLE.TODO} SET todo=?, isCompleted=? WHERE id=?`,
-      [
-        todo,
-        isCompleted,
-        _id,
-      ],
-    )
-    // return count of rows updated
-    return result.affectedRows
+  updateById: async ({ _id, ...bankAccount }: BankAccount) => {
+    const bankAccountExists = await bankAccounts.findOne({ _id: _id });
+    if (!bankAccountExists) {
+      return null;
+    }
+    const { matchedCount, modifiedCount } = await bankAccounts.updateOne(
+      { _id },
+      { $set: bankAccount },
+    );
+    if (matchedCount && modifiedCount) {
+      return await bankAccounts.findOne({ _id: _id });
+    }
+    return null;
   },
 
   /**
-   * Deletes a todo by ID
-   * @param id
-   * @returns integer (count of effect rows)
+   * @description Deletes a bank account by ID
+   *
+   * @param id account to delete
+   *
+   * @returns count of deleted bank accounts
    */
   deleteById: async ({ _id }: BankAccount) => {
-    const result = await client.query(
-      `DELETE FROM ${TABLE.TODO} WHERE id = ?`,
-      [id],
-    )
-    // return count of rows updated
-    return result.affectedRows
+    const { deletedCount } = await bankAccounts.deleteOne({
+      _id: _id,
+    });
+    return deletedCount;
   },
-}
+};
