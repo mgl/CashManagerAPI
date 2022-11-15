a2e00dbc9e063058855d5443d69685f1554b50f3
mgl <marius.glo@proton.me>
2022-11-15 10:27:23 +0000

    Added Mongo connection

--- deps.ts
+++ deps.ts
@@ -3,4 +3,6 @@
  *
  * This module re-exports the required methods from the dependant remote Ramda module.
  */
-export { Application, Router } from "https://deno.land/x/oak/mod.ts";
+export { config } from "https://deno.land/x/dotenv@v3.2.0/mod.ts";
+export { Bson, MongoClient } from "https://deno.land/x/mongo@v0.31.1/mod.ts";
+export { Application, Router } from "https://deno.land/x/oak@v11.1.0/mod.ts";
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,19 @@
 import { Application, Router } from "../deps.ts";
+import { MongoClient } from "../deps.ts";
 
-const router = new Router();
-const port = 8080;
+const router: Router = new Router();
+const port: number = Deno.env.get("PORT") ? parseInt(Deno.env.get("PORT")!) : 3000;
+
 router
   .get("/", (context) => {
-    context.response.body = "CashManager API test commit";
-  })
+    context.response.body = "CashManager API";
+  });
+
+// Connect to MongoDB
+const client = new MongoClient();
+const mongoURI: string = Deno.env.get("DB_URI") ? Deno.env.get("DB_URI")! : "mongodb://localhost:27017";
+client.connect(mongoURI);
+console.log(client.listDatabases());
 
 const app = new Application();
 app.use(router.routes());
