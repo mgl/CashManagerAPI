7b29b272a359c6275239407109f817846d5a1448
mgl <marius.glo@proton.me>
2022-11-28 11:22:57 +0000

    Fixed linting

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -4,6 +4,7 @@ import Transactions from "../controllers/transactions.ts";
 const router = new Router();
 
 router
+  .get("/api/transactions", Transactions.getAll)
   .post("/api/transactions", Transactions.createTransaction);
 
 export default router;
