94b5ee158168e53beff292eb4f54ec6cd83c4f1f
quent <quentin.andreani@epsi.fr>
2022-11-28 09:59:57 +0000

    Correction de linter

--- tests/bank_accounts_test.ts
+++ tests/bank_accounts_test.ts
@@ -1,19 +1,19 @@
-import { assertEquals} from "https://deno.land/std@0.165.0/testing/asserts.ts";
+import { assertEquals } from "https://deno.land/std@0.165.0/testing/asserts.ts";
 
-const baseUrl = "http://localhost:8080"
+const baseUrl = "http://localhost:8080";
 
 Deno.test("Create Account", async () => {
   const body = {
-    "account_number" : 12966372819,
-    "firstname" : "Ichigo",
-    "lastname" : "Kurosaki",
-    "balance" : 100
+    "account_number": 12966372819,
+    "firstname": "Ichigo",
+    "lastname": "Kurosaki",
+    "balance": 100,
   };
 
-  const response = await fetch(baseUrl + '/api/accounts', {
+  const response = await fetch(baseUrl + "/api/accounts", {
     method: "POST",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
     body: JSON.stringify(body),
   });
@@ -24,16 +24,16 @@ Deno.test("Create Account", async () => {
 
 Deno.test("Create Account Already Created", async () => {
   const body = {
-    "account_number" : 12966372819,
-    "firstname" : "Ichigo",
-    "lastname" : "Kurosaki",
-    "balance" : 100
+    "account_number": 12966372819,
+    "firstname": "Ichigo",
+    "lastname": "Kurosaki",
+    "balance": 100,
   };
 
-  const response = await fetch(baseUrl + '/api/accounts', {
+  const response = await fetch(baseUrl + "/api/accounts", {
     method: "POST",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
     body: JSON.stringify(body),
   });
@@ -42,35 +42,35 @@ Deno.test("Create Account Already Created", async () => {
   assertEquals(jsonResponse.success, false);
 });
 
-Deno.test("Get All Account", async() => {
-  const response = await fetch(baseUrl + '/api/accounts');
+Deno.test("Get All Account", async () => {
+  const response = await fetch(baseUrl + "/api/accounts");
   const jsonResponse = await response.json();
   assertEquals(jsonResponse.success, true);
 });
 
-Deno.test("Get Account by num account", async() => {
-  const response = await fetch(baseUrl + '/api/accounts/12966372819');
+Deno.test("Get Account by num account", async () => {
+  const response = await fetch(baseUrl + "/api/accounts/12966372819");
   const jsonResponse = await response.json();
   assertEquals(jsonResponse.success, true);
 });
 
-Deno.test("Get Account by num account not found", async() => {
-  const response = await fetch(baseUrl + '/api/accounts/1296632819');
+Deno.test("Get Account by num account not found", async () => {
+  const response = await fetch(baseUrl + "/api/accounts/1296632819");
   const jsonResponse = await response.json();
   assertEquals(jsonResponse.success, false);
 });
 
-Deno.test("Update Account by num account", async() => {
+Deno.test("Update Account by num account", async () => {
   const body = {
-    "firstname" : "Ichigo",
-    "lastname" : "Kurosaki",
-    "balance" : 100
+    "firstname": "Ichigo",
+    "lastname": "Kurosaki",
+    "balance": 100,
   };
 
-  const response = await fetch(baseUrl + '/api/accounts/12966372819', {
+  const response = await fetch(baseUrl + "/api/accounts/12966372819", {
     method: "PUT",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
     body: JSON.stringify(body),
   });
@@ -79,12 +79,12 @@ Deno.test("Update Account by num account", async() => {
   assertEquals(jsonResponse.success, true);
 });
 
-Deno.test("Delete Account by num account", async() => {
-  const response = await fetch(baseUrl + '/api/accounts/12966372819', {
+Deno.test("Delete Account by num account", async () => {
+  const response = await fetch(baseUrl + "/api/accounts/12966372819", {
     method: "DELETE",
     headers: {
-      'Content-Type': 'application/json',
-    }
+      "Content-Type": "application/json",
+    },
   });
 
   const jsonResponse = await response.json();
