5212c24e87671de88e0289aa8f899733b79719ba
mgl <marius.glo@proton.me>
2022-12-20 09:48:29 +0000

    Update Deno and deps

--- deps.ts
+++ deps.ts
@@ -3,7 +3,7 @@
  *
  * This module re-exports the required methods.
  */
-import "https://deno.land/std@0.167.0/dotenv/load.ts";
+import "https://deno.land/std@0.170.0/dotenv/load.ts";
 export { Bson, MongoClient } from "https://deno.land/x/mongo@v0.31.1/mod.ts";
 export {
   Application,
@@ -19,7 +19,7 @@ export {
   green,
   red,
   yellow,
-} from "https://deno.land/std@0.167.0/fmt/colors.ts";
+} from "https://deno.land/std@0.170.0/fmt/colors.ts";
 export { format } from "https://deno.land/std@0.167.0/datetime/mod.ts";
 export { Request } from "https://deno.land/x/oak@v11.1.0/request.ts";
 export { Response } from "https://deno.land/x/oak@v11.1.0/response.ts";
@@ -31,4 +31,4 @@ export {
 } from "https://deno.land/x/djwt@v2.8/mod.ts";
 export type { Header, Payload } from "https://deno.land/x/djwt@v2.8/mod.ts";
 export * as bcrypt from "https://deno.land/x/bcrypt@v0.4.1/mod.ts";
-export * as mod from "https://deno.land/std@0.167.0/node/os.ts";
+export * as mod from "https://deno.land/std@0.170.0/node/os.ts";
